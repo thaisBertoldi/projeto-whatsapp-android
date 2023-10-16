2073f23894a1510588b0b9b9cc76bcea08320e85
thaisBertoldi <thais_brt@hotmail.com>
2023-10-16 18:36:36 -0300

    feat: metodo sair

--- app/src/main/java/com/thais/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/thais/whatsapp/activity/MainActivity.java
@@ -8,14 +8,19 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.thais.whatsapp.R;
+import com.thais.whatsapp.config.ConfigFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = ConfigFirebase.getFirebaseAuth();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("WhatsApp");
@@ -27,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menuSair) {
+            deslogarUsuario();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario() {
+        try {
+            auth.signOut();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
